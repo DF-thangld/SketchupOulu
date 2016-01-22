72a0ef053cb976e8c355b10b7f770ad9eb5ed6e2
ZJJ1991 <zhoujunjie.10@gmail.com>
2016-01-22 16:15:35 +0200

    some test functions are added

--- unit_tests.py
+++ unit_tests.py
@@ -11,6 +11,8 @@ class SketchupOuluUnitTest(TestCase):
 
     def create_app(self):
         app.config['WTF_CSRF_ENABLED'] = False
+        with app.app_context():
+            g.user = None
         return app
 
     def setUp(self):
@@ -31,10 +33,12 @@ class SketchupOuluUnitTest(TestCase):
             resp = self.client.post('users/logout', follow_redirects=True)
 		
     def login_as_admin(self):
-        self.login('df.thangld@hotmail.com', 'abc')
+        with app.app_context():
+            self.login('df.thangld@hotmail.com', 'abc')
 		
     def login_as_user(self):
-        self.login('dreamingfighter@gmail.com', 'abc')
+        with app.app_context():
+            self.login('dreamingfighter@gmail.com', 'abc')
 
     def test_login_sucess(self):
         input_data = {
@@ -54,6 +58,52 @@ class SketchupOuluUnitTest(TestCase):
         with app.app_context():
             resp = self.client.post('users/login/', data=input_data)
             self.assertNotIn('user', g)
+
+    def register_successful(self):
+        input_data = {
+            "name" : "Junjie1991",
+            "email" : "zhoujunjie.10@gmail.com",
+            "password" : "1234",
+            "confirm" : "1234"
+            "read"
+        }
+        with app.app_context():
+            resp = self.client.post('users/register/', data=input_data)
+            user = User.query.filter_by(username=input_data['name'], email=input_data['email']).first()
+            self.assertNotEqual(user, None)
+
+    def test_login_notfound(self):
+        input_data = {"email": "sadasd",
+                      "password": "abc"}
+        with app.app_context():
+            resp = self.client.post('users/login/', data=input_data)
+            self.assertNotIn('user', g)
+
+    def test_login_wrongpassword(self):
+        input_data = {"email": "df.thangld@hotmail.com",
+            "password": "abcd"
+                    }
+        with app.app_context():
+            resp = self.client.post('users/login/', data=input_data)
+            self.assertNotIn('user', g)
+
+
+    def test_upload_profile(self):
+        input_data = {
+            "address" : "Vietnam",
+            "fullname" : "Luu Thang",
+            "phone_number" : "12345"
+        }
+        with app.app_context():
+            #to login, use this line of code
+            self.client.post('users/login/', data={'email': 'df.thangld@hotmail.com', 'password': 'abc'})
+            resp = self.client.post('users/user_profile/', data=input_data)
+            user = User.query.filter_by(address=input_data['address']).first()
+            self.assertNotEqual(user, None)
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
