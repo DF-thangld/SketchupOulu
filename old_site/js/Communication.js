65961826743e40dc0a7b557df83a972d03d0cda1
Pingjiang Li <lpj614@hotmail.com>
2016-02-01 11:11:03 +0200

    add function to load predefined objects

--- old_site/js/Communication.js
+++ old_site/js/Communication.js
@@ -50,6 +50,52 @@ var Communicator = {
 	return ModelsArray;
 
 	},
+	getSceneFromDB: function (parameters) {
+
+	var model_0= {
+		modelName:"building_1",
+		index:"0",
+		x:0,
+		y:0,
+		z:0,
+	};
+	var model_1= {
+		modelName:"building_2",
+		index:"1",
+		x:300,
+		y:100,
+		z:0,
+	};
+	/*var model_2= {
+		modelName:"tree_1",
+		objPath:'./object/tree_1/tree_1.obj',
+		mtlPath:'./object/tree_1/tree_1.mtl',
+		position_x:0,
+		position_y:0,
+	};
+	var model_3= {
+		modelName:"terrain_1",
+		objPath:'./object/terrain_1/terrain_1.obj',
+		mtlPath:'./object/terrain_1/terrain_1.mtl',
+		position_x:0,
+		position_y:0,
+	};
+	var model_2= {
+		modelName:"Church",
+		modelPath:'./collada/Church.dae',
+		position_x:-200,
+		position_y:0,
+	};*/
+
+	var ModelsArray = [];
+	ModelsArray[0]=model_0;
+	ModelsArray[1]=model_1;
+	//ModelsArray[2]=model_2;
+	//ModelsArray[3]=model_3;
+
+	return ModelsArray;
+
+	},
 
 	updateModelsToDB: function ( parameters ) {
 
