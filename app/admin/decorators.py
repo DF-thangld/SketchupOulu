3353d2eb08a2a160db1dc6db37a0407a7a06c169
df-thangld <DF.thangld@hotmail.com>
2016-02-21 22:38:39 +0200

    fix the bug that make normal user can access admin function

--- app/admin/decorators.py
+++ app/admin/decorators.py
@@ -5,7 +5,7 @@ from flask import g, flash, redirect, url_for, request
 def requires_admin(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if g.user is None and not g.user.is_admin():
+        if 'user' not in g or g.user is None or not g.user.is_admin():
             flash(u'You don\'t have privilege to view this page.')
             return redirect(url_for('users.login', next=request.path))
         return f(*args, **kwargs)
