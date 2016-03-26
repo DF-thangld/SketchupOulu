ffef58c5fe85d399c5a3ae80af45ac4094db6b5e
df-thangld <DF.thangld@hotmail.com>
2016-03-26 14:13:36 +0200

    clean repository

--- old_site/install_db.py
+++
@@ -1,8 +0,0 @@
-import variables
-from models.user import User
-from models.scenario import Scenario
-from models.custom_model import CustomModel
-from models.comment import CommentTopic
-#from models.comment import Comment
-
-variables.db.create_all()
--- old_site/variables.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import sqlalchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://sketchup_oulu:abcxyz@vm0106.virtues.fi:7557/sketchup_oulu'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
