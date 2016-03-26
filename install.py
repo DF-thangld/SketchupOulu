bb82c43e5d07302fed01b9ed81878fd7a7270672
df-thangld <DF.thangld@hotmail.com>
2016-03-26 14:20:39 +0200

    add version tag to install

--- install.py
+++ install.py
@@ -2,6 +2,7 @@ from app import app, db
 import app.users.models as users
 import app.sketchup.models as sketchup
 import app.journal.models as journal
+import app.admin.models as admin
 print('create database schema')
 db.create_all()
 
@@ -12,7 +13,6 @@ user_group = users.Group('Public Users', '')
 user_group.id = 2
 db.session.add(admin_group)
 db.session.add(user_group)
-db.session.commit()
 
 print('create first admin user')
 user = users.User(username='admin', email=config.EMAIL['address'], password='abcxyz')
@@ -20,5 +20,13 @@ user.banned = 0
 user.groups.append(admin_group)
 user.groups.append(user_group)
 db.session.add(user)
+
+print ('create version tag')
+version_tag = admin.SystemParameter()
+version_tag.name = 'version'
+version_tag.value = '1.00'
+db.session.add(version_tag)
+
+print('commit to database')
 db.session.commit()
 print('finish')
